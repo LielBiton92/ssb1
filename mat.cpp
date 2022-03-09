471498f79abc921b657e4805f52f1e25484eb9ce
LielBiton92 <92746221+LielBiton92@users.noreply.github.com>
2022-03-09 03:56:59 -0800

    Add files via upload

---
+++ mat.cpp
@@ -0,0 +1,11 @@
+#include "string"
+#include "iostream"
+using namespace std;
+#include "mat.hpp"
+using namespace ariel;
+
+    string ariel::mat(int colls , int rows , char a, char b){
+        return "2";
+    }
+  
+    
